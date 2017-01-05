fd84d134c014073079681220aaa66b98f1516fb7
Bradley Gaynor <bradleygaynor@northwesternmutual.com>
2017-01-05 16:03:01 -0600

    update build stuff

--- run.js
+++ run.js
@@ -97,43 +97,25 @@ tasks.set('build', () => {
 // Build and publish the website
 // -----------------------------------------------------------------------------
 tasks.set('publish', () => {
-    const remote = {
-        url: 'https://github.com/Bmgaynor/Bmgaynor.github.io.git', // TODO: Update deployment URL
-        branch: 'master',
-    };
-    global.DEBUG = process.argv.includes('--debug') || false;
-    const spawn = require('child_process').spawn;
-    const opts = { cwd: path.resolve(__dirname, './build'), stdio: ['ignore', 'inherit', 'inherit'] };
-    const git = (...args) => new Promise((resolve, reject) => {
-        spawn('git', args, opts).on('close', code => {
-            if (code === 0) {
-                resolve();
-            } else {
-                reject(new Error(`git ${args.join(' ')} => ${code} (error)`));
-            }
-        });
+  const ghPages = require('gh-pages');
+  global.DEBUG = process.argv.includes('--debug') || false;
+  const publish = (dir) => new Promise((resolve, reject) => {
+    ghPages.publish(dir, {}, (err) => {
+      if (err) {
+        reject();
+      } else {
+        resolve();
+      }
     });
+  });
 
-    return Promise.resolve()
-        .then(() => run('clean'))
-        .then(() => git('init', '--quiet'))
-        .then(() => git('config',  '--get', 'remote.origin.url')
-            .then(() => git('remote', 'set-url', 'origin', remote.url))
-            .catch(() => git('remote', 'add', 'origin', remote.url))
-        )
-        .then(() => git('ls-remote', '--exit-code', remote.url, 'master')
-            .then(() => Promise.resolve()
-                .then(() => git('fetch', 'origin'))
-                .then(() => git('reset', `origin/${remote.branch}`, '--hard'))
-                .then(() => git('clean', '--force'))
-            )
-            .catch(() => Promise.resolve())
-        )
-        .then(() => run('build'))
-        .then(() => git('add', '.', '--all'))
-        .then(() => git('commit', '--message', new Date().toUTCString())
-            .catch(() => Promise.resolve()))
-        .then(() => git('push', 'origin', `HEAD:${remote.branch}`, '--force', '--set-upstream'));
+  return Promise.resolve()
+    .then(() => run('clean'))
+    .then(() => run('build'))
+    .then(() => publish(`${__dirname}/public`), {
+      branch: 'master',
+      repo: 'https://github.com/Bmgaynor/Bmgaynor.github.io.git'
+    });
 });
 //
 // Build website and launch it in a browser for testing (default)

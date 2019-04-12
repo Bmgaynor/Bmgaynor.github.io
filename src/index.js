4086b29f4dc0408caf4275b646d47f204ae6ca4b
Bradley Gaynor <bradleygaynor@northwesternmutual.com>
2019-04-12 15:22:31 -0500

    Update to use react-scripts

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>Hello from react</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));

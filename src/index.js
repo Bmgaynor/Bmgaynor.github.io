3cdfd2c0e65f92a48469363c0fd90f883f5628d8
Bradley Gaynor <BradleyGaynor@northwesternmutual.com>
2020-03-09 15:02:16 -0500

    add links

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className="container">
         <div className="portrait-container">
-          <img className="portrait" src="./Me.jpg" />
+          <img className="portrait" src="./Me.jpg" alt="Brad Gaynor" />
         </div>
         <h1>Bradley Gaynor</h1>
         <h3>Senior Software Engineer</h3>

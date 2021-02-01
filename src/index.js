d45f3a14c7113c84a2febdbe70f68457edbfef7f
Bradley Gaynor <BradleyGaynor@northwesternmutual.com>
2021-02-01 13:30:34 -0800

    update photo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Links from './Links'
+import portrait from './images/BradGaynor.jpg'
 
 const App = () => {
   return (
     <div className="container">
         <div className="portrait-container">
-          <img className="portrait" src="./Me.jpg" alt="Brad Gaynor" />
+          <img className="portrait" src={portrait} alt="Brad Gaynor" />
         </div>
         <h1>Bradley Gaynor</h1>
         <h3>Senior Software Engineer</h3>

8b467bb8477b1f165ac02f707c2d59d5932141b9
bradley gaynor <bradmgaynor@gmail.com>
2022-09-29 12:45:28 -0500

    update photo and add readme

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Personal Site
+
+Assets hosted with github pages exposed under my google domain bradgaynor.com
+
+## Dev
+
+```bash
+    npm install
+    npm start
+```
+
+## Deploy
+
+CI/CD is set up with github actions. To deploy merge to master.
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Links from './Links'
-import portrait from './images/BradGaynor.jpg'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Links from "./Links";
+import portrait from "./images/Me2.jpg";
 
 const App = () => {
   return (
     <div className="container">
-        <div className="portrait-container">
-          <img className="portrait" src={portrait} alt="Brad Gaynor" />
-        </div>
-        <h1>Bradley Gaynor</h1>
-        <h3>Senior Software Engineer</h3>
-        <Links />
+      <div className="portrait-container">
+        <img className="portrait" src={portrait} alt="Brad Gaynor" />
+      </div>
+      <h1>Bradley Gaynor</h1>
+      <h3>Senior Software Engineer</h3>
+      <Links />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
